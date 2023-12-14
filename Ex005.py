6590a932131b8a838cacad9115103fd94f55689e
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-14 11:52:09 -0300

    005 - Antecessor e Sucessor

---
+++ Ex005.py
@@ -0,0 +1,6 @@
+""""Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor."""
+
+n = int(input("Digite um número: "))
+ante = n - 1
+suce = n + 1
+print(f"O antecessor de {n} é {ante} e o sucessor é {suce}")
