05462294e3b8c45fa677f8208b089f6254488725
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-16 11:32:42 -0300

    Pintando parede

--- Mundo1/Ex011.py
+++ Mundo1/Ex011.py
@@ -2,7 +2,7 @@
 largura = float(input("Digite a largura em metros da parede: "))
 altura = float(input("Digite a altura em metros da parede: "))
 area = largura * altura
-print(f"Sua parede tem a dimensão de {largura}x{altura} e sua area é de {area}m²")
+print(f"Sua parede tem a dimensão de {largura}x{altura} e sua area é de {area}m².")
 tinta = area / 2
-print(f"Para pintar esta parede de {area}m² você vai precisar de {tinta} litros de tinta")
+print(f"Para pintar esta parede de {area}m² você vai precisar de {tinta} litros de tinta.")
 
