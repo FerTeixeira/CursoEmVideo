f894bd2211a91fd32482dbde4462b66bc3bb03c4
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-16 11:35:30 -0300

    Conversor de temperaturas

--- Mundo1/Ex014.py
+++ Mundo1/Ex014.py
@@ -1,4 +1,4 @@
 """"Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit."""
 temperatura = float(input("Digete a temperatura em Celsius: "))
-fahrenheit = ((9 * temperatura)/5) + 32
+fahrenheit = ((9 * temperatura) / 5) + 32
 print(f"A temperatura de {temperatura} Celsius corresponde a {fahrenheit} Fahrenheit.")
