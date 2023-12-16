da60b95ef9142c09a1442099c194c3a2720bf32f
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-16 11:43:48 -0300

    Separando dígitos de um número

--- Mundo1/Ex023.py
+++ Mundo1/Ex023.py
@@ -4,8 +4,8 @@ unidade = n // 1 % 10
 dezena = n // 10 % 10
 centena = n // 100 % 10
 milhar = n // 1000 % 10
-print(f"Analisando o número {n}")
-print(f"Unidade: {unidade}")
-print(f"Dezena: {dezena}")
-print(f"Centena: {centena}")
-print(f"Milhar: {milhar}")
+print(f"Analisando o número {n}.")
+print(f"Unidade: {unidade}.")
+print(f"Dezena: {dezena}.")
+print(f"Centena: {centena}.")
+print(f"Milhar: {milhar}.")
