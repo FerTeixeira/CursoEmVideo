88923d2d1bb25c4b22008d3b61ec70a6e3efc1d0
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-16 09:42:28 -0300

    Dissecando uma variável

--- Mundo1/Ex004.py
+++ Mundo1/Ex004.py
@@ -2,10 +2,10 @@
 
 algo = input("Digite algo: ")
 
-print(f"Qual o tipo primitivo de {algo}? {type(algo)}")
-print(f"{algo} é um número? {algo.isnumeric()}")
-print(f"{algo} é uma string? {algo.isalpha()}")
-print(f"{algo} é alfanumérico? {algo.isalnum()}")
-print(f"{algo} está escrito em maiúsculo? {algo.isupper()}")
-print(f"{algo} está escrito em minúsculo? {algo.islower()}")
-print(f"{algo} só tem espaços? {algo.isspace()}")
+print(f"Qual o tipo primitivo de {algo}? {type(algo)}.")
+print(f"{algo} é um número? {algo.isnumeric()}.")
+print(f"{algo} é uma string? {algo.isalpha()}.")
+print(f"{algo} é alfanumérico? {algo.isalnum()}.")
+print(f"{algo} está escrito em maiúsculo? {algo.isupper()}.")
+print(f"{algo} está escrito em minúsculo? {algo.islower()}.")
+print(f"{algo} só tem espaços? {algo.isspace()}.")
