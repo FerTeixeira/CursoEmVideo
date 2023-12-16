8b9f8e9c53740a8ff801d8e0accf51c95b7ee63e
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-16 11:42:22 -0300

    Analisador de textos

--- Mundo1/Ex022.py
+++ Mundo1/Ex022.py
@@ -2,8 +2,9 @@
 – O nome com todas as letras maiúsculas e minúsculas.
 – Quantas letras ao todo (sem considerar espaços).
 – Quantas letras tem o primeiro nome."""
+
 nome = str(input("Digite seu nome completo: ")).strip()
-print(f"O nome {nome} com as letras MAIÚSCULAS é: {nome.upper()}")
-print(f"O nome {nome} com letras MINÚSCULAS é: {nome.lower()}")
-print(f"O nome {nome} tem {len(nome) - nome.count(' ')} letras")
-print(f"Seu primeiro nome tem {nome.find(' ')} lestras")
+print(f"O nome {nome} com as letras MAIÚSCULAS é: {nome.upper()}.")
+print(f"O nome {nome} com letras MINÚSCULAS é: {nome.lower()}.")
+print(f"O nome {nome} tem {len(nome) - nome.count(' ')} letras.")
+print(f"Seu primeiro nome tem {nome.find(' ')} lestras.")
