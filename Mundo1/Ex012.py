c3e26df66a21f57ad7b43b81145b5ca0387a4373
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-16 11:34:06 -0300

    Calculando descontos

--- Mundo1/Ex012.py
+++ Mundo1/Ex012.py
@@ -2,4 +2,4 @@
 preco = float(input("Digite o preço de produto: R$"))
 desconto = preco - preco * (5/100)
 
-print(f"O produto que custava R${preco:.2f}, na promoção com desconto de 5% vai custar R${desconto:.2f}")
+print(f"O produto que custava R${preco:.2f}, na promoção com desconto de 5% vai custar R${desconto:.2f}.")
