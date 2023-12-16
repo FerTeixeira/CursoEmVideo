eb3726b30173b7753629d6b08f9059e24c8b3313
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-16 09:40:47 -0300

    Respondendo ao Usuário

--- Mundo1/Ex002.py
+++ Mundo1/Ex002.py
@@ -1,4 +1,4 @@
 """Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas."""
 
 nome = str(input("Digete seu nome: "))
-print(f"É um prazer te conhecer, {nome}!!")
+print(f"É um prazer te conhecer, {nome}!")
