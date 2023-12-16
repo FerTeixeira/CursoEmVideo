1a2607992b93aa3ae2fde21da396a2b6780becce
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-16 11:39:28 -0300

    Seno, cosseno e tangente

--- Mundo1/Ex018.py
+++ Mundo1/Ex018.py
@@ -4,4 +4,6 @@ angulo = float(input("Digite a ângulo que você precisa: "))
 seno = math.sin(math.radians(angulo))
 cosseno = math.cos(math.radians(angulo))
 tangente = math.tan(math.radians(angulo))
-print(f"O angulo de {angulo} tem seno {seno:.2f}, cosseno {cosseno:.2f} e tangente {tangente:.2f}.")
+print(f"O angulo de {angulo} tem seno {seno:.2f}, "
+      f"cosseno {cosseno:.2f} e "
+      f"tangente {tangente:.2f}.")
