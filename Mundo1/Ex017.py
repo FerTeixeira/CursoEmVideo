9f9b8702a228f0ad187c03dd6a8a41e355503ef5
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-16 11:37:27 -0300

    Catetos e hipotenusas

--- Mundo1/Ex017.py
+++ Mundo1/Ex017.py
@@ -2,4 +2,4 @@
 co = float(input("Comprimento do cateto oposto: "))
 ca = float(input("Comprimento do cateto adjacente: "))
 hi = (co ** 2 + ca ** 2) ** (1/2)
-print(f"A hipotenusa vai medir {hi:.2f}")
+print(f"A hipotenusa vai medir {hi:.2f}.")
