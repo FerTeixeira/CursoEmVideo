bba275b7fb727af8e6a7721f8af1309efcf25dcd
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-17 11:26:26 -0300

    Jogo da Adivinhação v.1.0

--- Mundo1/Condicoes1/Ex028.py
+++ Mundo1/Condicoes1/Ex028.py
@@ -1,1 +1,9 @@
 """Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu."""
+from random import randint
+n = randint(0, 5)
+print("Vou pensar em um número entre 0 e 5. Tente adivinhar...")
+r = int(input("Digite o número que eu pensei: "))
+if n == r:
+    print("Você acertou")
+else:
+    print(f"Você errou! Eu pensei no {n}")
