7bd3d3b81bd8c16490ad4a0596cbb33713f3341a
FerTeixeira <fer-teixeirandrade@hotmail.com>
2023-12-19 12:52:27 -0300

    Aquele clássico da Média

---
+++ Mundo2/2.1CondicoesAninhadas/Ex040.py
@@ -0,0 +1,18 @@
+"""Crie um programa que leia duas notas de um aluno e calcule sua
+média, mostrando uma mensagem no final, de acordo com a média atingida:
+– Média abaixo de 5.0: REPROVADO
+
+– Média entre 5.0 e 6.9: RECUPERAÇÃO
+
+– Média 7.0 ou superior: APROVADO"""
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+m = (n1 + n2) / 2
+print(f"Sua média é {m:.2f}")
+if m < 5:
+    print("Você está REPROVADO!")
+elif m >= 5 and m < 7:
+    print("Você está de RECUPERAÇÃO!")
+else:
+    print("Você está APROVADO!")
